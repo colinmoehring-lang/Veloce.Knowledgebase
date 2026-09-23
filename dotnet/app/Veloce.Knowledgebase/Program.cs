7d3a2f7954914efd3a59dfa48bf9199866b04c4f
Colin Moehring <colin.moehring@de.abb.com>
2026-09-23 09:16:34 +0200

    Enable swagger UI in all environments

--- dotnet/app/Veloce.Knowledgebase/Program.cs
+++ dotnet/app/Veloce.Knowledgebase/Program.cs
@@ -68,14 +68,11 @@ using (var scope = app.Services.CreateScope())
     dbContext.Database.Migrate();
 }
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Veloce API v1");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Veloce API v1");
+});
 
 app.UseRouting();
 
